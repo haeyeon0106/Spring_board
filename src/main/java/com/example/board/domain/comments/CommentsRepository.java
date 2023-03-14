4e178dfdf0285983775903ec9a92c7f8c5496d2a
haeyeon0106 <yeon99016@gmail.com>
2023-03-14 13:38:51 +0900

    파일삭제 처리

--- src/main/java/com/example/board/domain/comments/CommentsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.board.domain.comments;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentsRepository extends JpaRepository<Comments,Long> {
-}
--- src/main/java/com/example/board/domain/posts/PostsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.board.domain.posts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {
-
-}
